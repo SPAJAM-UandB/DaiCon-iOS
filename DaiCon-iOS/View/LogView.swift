89c58faeec1d544b4d643ba38669384664769264
中岡黎 <reideeplearning@gmail.com>
2020-11-01 23:52:00 +0900

    first commit

---
+++ DaiCon-iOS/View/LogView.swift
@@ -0,0 +1,23 @@
+//
+//  LogView.swift
+//  DaiCon-iOS
+//
+//  Created by 中岡黎 on 2020/11/01.
+//
+
+import SwiftUI
+
+struct LogView: View {
+    var body: some View {
+        NavigationView {
+            Text("log View")
+                .navigationTitle("Life Log")
+        }
+    }
+}
+
+struct LogView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogView()
+    }
+}
